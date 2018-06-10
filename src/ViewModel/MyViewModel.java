fa2d1e3c2733ebce694874a1def2c4738cfec0fc
itzik <itzikd15@gmail.com>
2018-06-10 13:42:34 +0300

    Part 3 Start

--- src/ViewModel/MyViewModel.java
+++ src/ViewModel/MyViewModel.java
@@ -1,4 +1,32 @@
 package ViewModel;
 
+import Model.IModel;
+import View.MazeDisplay;
+
 public class MyViewModel {
+    private IModel mode;
+    public MazeDisplay mazeDisplayer;
+
+    public MyViewModel(IModel mode) {
+        this.mode = mode;
+    }
+
+
+    public void generateMaze(int width, int height) {
+        mode.generateMaze(width,height);
+    }
+
+
+    /**
+     * check if string is an int
+     */
+    private int checkInt(String check) {
+        try {
+            int temp = Integer.parseInt(check); //use your variable or object in place of obj
+            return temp;
+        }
+        catch (NumberFormatException e) {
+            return 10;
+        }
+    }
 }
