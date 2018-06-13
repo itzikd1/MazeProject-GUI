1acd2415871a7d06387bf8c3c5853fdeaa6d6640
itzikd1 <vhhchh20>
2018-06-13 09:45:42 +0300

    j

--- src/View/MyViewController.java
+++ src/View/MyViewController.java
@@ -1,4 +1,110 @@
 package View;
+import ViewModel.MyViewModel;
+import algorithms.mazeGenerators.IMazeGenerator;
+import algorithms.mazeGenerators.Maze;
+import algorithms.mazeGenerators.MyMazeGenerator;
+import algorithms.search.*;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.EventHandler;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
-public class MyViewController implements IView {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Aviadjo on 3/9/2017.
+ */
+public class MyViewController extends Canvas {
+
+    private Maze maze;
+    private int characterPositionRow = 1;
+    private int characterPositionColumn = 1;
+
+    public int getCharacterPositionRow() {
+        return characterPositionRow;
+    }
+    public int getCharacterPositionColumn() {
+        return characterPositionColumn;
+    }
+
+    public void setMaze(int rows, int columns) {
+        IMazeGenerator mg = new MyMazeGenerator();
+        Maze x = mg.generate(rows, columns);
+        this.maze = x;
+        redraw();
+    }
+
+    public void setCharacterPosition(int row, int column) {
+        characterPositionRow = row;
+        characterPositionColumn = column;
+        redraw();
+    }
+    
+    public void redraw() {
+        if (maze != null) {
+            double canvasHeight = getHeight();
+            double canvasWidth = getWidth();
+            double cellHeight = canvasHeight / maze.numOfRows();
+            double cellWidth = canvasWidth / maze.numOfColumns();
+
+            try {
+                GraphicsContext graphicsContext2D = getGraphicsContext2D();
+                graphicsContext2D.clearRect(0, 0, getWidth(), getHeight()); //Clears the canvas
+                Image wallImage = new Image(new FileInputStream(ImageFileNameWall.get()));
+
+                //Draw Maze
+                for (int i = 0; i < maze.numOfRows(); i++) {
+                    for (int j = 0; j < maze.numOfColumns();j++) {
+                        if (maze.getCellValue(i,j) == 1) {
+                            //graphicsContext2D.fillRect(i * cellHeight, j * cellWidth, cellHeight, cellWidth);
+                            graphicsContext2D.drawImage(wallImage, i * cellHeight, j * cellWidth, cellHeight, cellWidth);
+                        }
+                    }
+                }
+
+                //Draw Character
+                //gc.setFill(Color.RED);
+                //gc.fillOval(characterPositionColumn * cellHeight, characterPositionRow * cellWidth, cellHeight, cellWidth);
+                Image characterImage = new Image(new FileInputStream(ImageFileNameCharacter.get()));
+                graphicsContext2D.drawImage(characterImage, characterPositionColumn * cellHeight, characterPositionRow * cellWidth, cellHeight, cellWidth);
+            } catch (FileNotFoundException e) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText(String.format("Image doesn't exist: %s",e.getMessage()));
+                alert.show();
+            }
+        }
+    }
+
+    //region Properties
+    private StringProperty ImageFileNameWall = new SimpleStringProperty();
+    private StringProperty ImageFileNameCharacter = new SimpleStringProperty();
+
+    public String getImageFileNameWall() {
+        return ImageFileNameWall.get();
+    }
+
+    public void setImageFileNameWall(String imageFileNameWall) {
+        this.ImageFileNameWall.set(imageFileNameWall);
+    }
+
+    public String getImageFileNameCharacter() {
+        return ImageFileNameCharacter.get();
+    }
+
+    public void setImageFileNameCharacter(String imageFileNameCharacter) {
+        this.ImageFileNameCharacter.set(imageFileNameCharacter);
+    }
+    //endregion
+
 }
