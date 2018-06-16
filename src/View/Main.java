163bbdfdddd232b1c53677918c18d1b19d1c6904
itzikd1 <vhhchh20>
2018-06-16 11:41:34 +0300

    Merge branch 'master' of https://github.com/itzikd1/Part3
    # Conflicts:
    #	src/Model/MyModel.java
    #	src/View/Main.java
    #	src/View/MazeDisplay.java
    #	src/ViewModel/MyViewModel.java

--- src/View/Main.java
+++ src/View/Main.java
@@ -19,12 +19,12 @@ import java.util.Optional;
 public class Main extends Application {
 
     //TODO DISPLAY SAVEGAME AFTER MAZE IS GENERATED( LOADED )
-    //TODO load and save - save works, load has error loading charcter\flag - need to save charcter and flag and then load them as well
     //TODO size of canvas isn't good
-    //TODO hint show 1 step foward, and there is a counter so we know what is the next step
+    //TODO hint still shows start position
+    // i dont think its necessary - player doesnt care which algo runs and what kind of maze it is.
     //TODO option,add what algorithem to run and what type of maze to create - need to connect controllers - display works
     //TODO change images to something nice, add backround maybe - css?
-    //TODO add music - what to do when song ends.
+    //TODO music - what to do when song ends, another song for finishing.
     //TODO add javadocs...ewww
     //TODO add a number of steps maybe on side
 
